2d79dbec8f5dff5e44bce31a2afb4e5e77aa027d
Ubuntu <ubuntu@ip-172-31-18-205.us-west-2.compute.internal>
2015-08-08 00:44:09 +0000

    fixed bug with mydotfiles path being wrong

--- node_setup/setup.sh
+++ node_setup/setup.sh
@@ -43,9 +43,9 @@ if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
 git clone https://github.com/kkhosravitest/mydotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+ln -sb mydotfiles/.screenrc .
+ln -sb mydotfiles/.bash_profile .
+ln -sb mydotfiles/.bashrc .
+ln -sb mydotfiles/.bashrc_custom .
+ln -sf mydotfiles/.emacs.d .
 
